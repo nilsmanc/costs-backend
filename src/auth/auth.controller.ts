29a8c4d57a0864869d5444b31c59558f63d27c96
nilsmanc <etereaiberia@gmail.com>
2022-09-08 16:19:18 +0300

    login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,9 +10,21 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { Response } from 'express';
 import { RegistrationGuard } from './guards/registration.guard';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoginGuard } from './guards/login.guard';
+
 @Controller('auth')
 export class AuthController {
   constructor(private usersService: UsersService) {}
+
+  @UseGuards(LoginGuard)
+  @Post('login')
+  async loginnUser(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
+    const user = await this.usersService.login(loginUserDto);
+    res.statusCode = HttpStatus.OK;
+    return res.send({ username: user.username });
+  }
+
   @UseGuards(RegistrationGuard)
   @Post('registration')
   async registartionUser(
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UsersDocument } from 'src/schemas/users.schema';
+import { LoginUserDto } from 'src/auth/dto/login-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User.name) private usersModel: Model<UsersDocument>,
   ) {}
+
+  async login(loginUserDto: LoginUserDto): Promise<User | null> {
+    const user = await this.usersModel.collection.findOne({
+      username: loginUserDto.username,
+    });
+
+    if (!user) {
+      return null;
+    }
+    return user as User;
+  }
 
   async registration(createUserDto: CreateUserDto): Promise<User | null> {
     const existingUser = await this.usersModel.collection.findOne({
