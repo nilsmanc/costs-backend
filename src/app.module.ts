15fe5c2a37131f48a37fd7b6335d1118c1009d4c
nilsmanc <etereaiberia@gmail.com>
2022-09-08 09:23:13 +0300

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { MongooseConfigService } from './config/MongooseConfigSerivce';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
 })
 export class AppModule {}
